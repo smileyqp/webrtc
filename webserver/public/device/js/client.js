f3ca9b3fe7fd984b0556ca53abf6cf7e07838832
smileyqp <468417415@qq.com>
2019-09-19 17:42:05 +0800

    choose devices on webpage

--- webserver/public/device/js/client.js
+++ webserver/public/device/js/client.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var audioSource = document.querySelector("select#audioSource");
+var audioOutput = document.querySelector("select#audioOutput");
+var videoSource = document.querySelector("select#videoSource");
+
 if( !navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices ){
     console.log('enumerateDevices is not support!')
 }else{
@@ -10,9 +14,18 @@ if( !navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices ){
 
 function gotDevices(deviceInfos) {
     deviceInfos.forEach((deviceInfo) => {
-        console.log(navigator)
-        console.log('00000000000000')
         console.log(deviceInfo.kind + ':label = ' + deviceInfo.label + ':id = ' + deviceInfo.deviceId + ':groupId = ' + deviceInfo.groupId);
+        var option = document.createElement('option');
+        option.value = deviceInfo.deviceId;
+        option.text = deviceInfo.label;
+        if(deviceInfo.kind === 'audioinput'){
+            audioSource.appendChild(option);
+        }else if(deviceInfo.kind === 'audiooutput'){
+            audioOutput.appendChild(option);
+        }else if(deviceInfo.kind === 'videoinput' ){
+            videoSource.appendChild(option);
+        }
+
     });
 }
 
