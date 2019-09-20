2292a2008789119b3dfc9b5546f4a83fcf605b03
smileyqp <468417415@qq.com>
2019-09-20 10:34:32 +0800

    获取流之后再去获取音视频设备名称

--- webserver/public/mediastream/js/client.js
+++ webserver/public/mediastream/js/client.js
@@ -2,10 +2,35 @@
 
 var videoplay = document.querySelector('video#player')
 
-function gotMediaStream (stream){
-    videoplay.srcObject = stream;
+
+//设备的展示与选择
+var audioSource = document.querySelector("select#audioSource");
+var audioOutput = document.querySelector("select#audioOutput");
+var videoSource = document.querySelector("select#videoSource");
+function gotDevices(deviceInfos) {      //参数deviceInfos是设备信息的数组
+    deviceInfos.forEach((deviceInfo) => {
+        console.log(deviceInfo.kind + ':label = ' + deviceInfo.label + ':id = ' + deviceInfo.deviceId + ':groupId = ' + deviceInfo.groupId);
+        var option = document.createElement('option');
+        option.value = deviceInfo.deviceId;
+        option.text = deviceInfo.label;
+        if(deviceInfo.kind === 'audioinput'){       //deviceInfo.kind来判断种类;音频
+            audioSource.appendChild(option);
+        }else if(deviceInfo.kind === 'audiooutput'){        //音频输出
+            audioOutput.appendChild(option);
+        }else if(deviceInfo.kind === 'videoinput' ){           //视频
+            videoSource.appendChild(option);
+        }
+
+    });
 }
 
+//获取到流
+function gotMediaStream (stream){
+    videoplay.srcObject = stream;
+    return navigator.mediaDevices.enumerateDevices();   //成功获取流；并返回一个promise；用于后边对设备的判断
+}
+
+//错误处理
 function handleError (err){
     console.log(err);
 }
@@ -18,7 +43,8 @@ if( !navigator.mediaDevices || !navigator.mediaDevices.getUserMedia ){
         audio : true
     }
     navigator.mediaDevices.getUserMedia(constraints)
-    .then(gotMediaStream)
+    .then(gotMediaStream)   //获取流
+    .then(gotDevices)       //设备获取处理
     .catch(handleError);
 }
 
